2d4e71f4ebfb3eaca4f9c137d6251dd83a2db47f
FranciscoJavierJim <225529106+FranciscoJavierJim@users.noreply.github.com>
2025-12-08 21:11:01 +0100

    correccion

--- cypress/02_POM/cypress.config.js
+++ cypress/02_POM/cypress.config.js
@@ -1,7 +1,14 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    baseUrl: 'http://localhost:3000',
+    
+    
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
+      return config
     },
   },
-};
+})
