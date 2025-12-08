cbda21550212d32d57e5313d63f33a54192ed5bd
FranciscoJavierJim <225529106+FranciscoJavierJim@users.noreply.github.com>
2025-12-08 22:02:12 +0100

    correccion

--- cypress/02_POM/cypress.config.js
+++ cypress/02_POM/cypress.config.js
@@ -1,11 +1,12 @@
-<<<<<<< HEAD
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.js',
     baseUrl: 'http://localhost:3000',
-    
+    video: true,  // asegura que se grabe video
+    screenshotsFolder: 'cypress/screenshots', // carpeta de screenshots
+    videosFolder: 'cypress/videos',           // carpeta de videos
     
     setupNodeEvents(on, config) {
       
